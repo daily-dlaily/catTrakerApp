147d330725dd80a2413c6e4dc3b94e262752a0d3
khouloud <dlaili.khouloud@gmail.com>
2021-09-25 11:12:59 +0100

    Created dashboard view

---
+++ src/components/dashboard/Dashboard.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import AddCat from '../Cats/AddCat'
+import CatsList from '../Cats/CatsList'
+
+const Dashboard = () => {
+    return (
+        <div>
+            <AddCat/>
+            <CatsList/>
+        </div>
+    )
+}
+
+export default Dashboard
